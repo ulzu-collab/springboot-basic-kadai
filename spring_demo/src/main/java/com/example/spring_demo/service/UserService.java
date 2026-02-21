12a9c7d5f3714584aa0f45cc046e7c04bb728a0b
ulzu-collab <ulzu@outlook.jp>
2026-02-21 20:53:28 +0900

    予約用クラスのカスケード削除を設定

--- spring_demo/src/main/java/com/example/spring_demo/service/UserService.java
+++ spring_demo/src/main/java/com/example/spring_demo/service/UserService.java
@@ -12,19 +12,38 @@ import com.example.spring_demo.repository.UserRepository;
 public class UserService {
 	private final UserRepository userRepository;
 	private final PasswordEncoder passwordEncoder;
-	
+
 	public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
 		this.userRepository = userRepository;
 		this.passwordEncoder = passwordEncoder;
 	}
-	
+
+	/**
+	 * 新規ログイン情報を登録する。今は仮の値を代入している。
+	 * @param signupForm
+	 * @return
+	 */
 	@Transactional
 	public UserEntity createUser(SignupForm signupForm) {
+
 		UserEntity userEntity = new UserEntity();
-		
+
 		userEntity.setEmail("admin@example.com");
-		
+		userEntity.setPassword("password");
+
 		return userRepository.save(userEntity);
 	}
+
+	public boolean authenticate(String email, String password) {
+
+		// 仮のユーザー情報（後でDBに置き換える）
+		String dummyEmail = "admin@example.com";
+		String dummyPassword = "password";
+
+		// email と password が一致しているかチェック
+		return dummyEmail.equals(email) && dummyPassword.equals(password);
+	}
+
+	// フォームクラスを作成、ログイン機能実装途中。
 
 }
