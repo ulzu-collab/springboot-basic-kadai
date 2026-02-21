e0439c4e647fcf13ab218c621e76e321f8346304
ulzu-collab <ulzu@outlook.jp>
2026-02-21 18:13:00 +0900

    ユーザー情報を検索して表示するメソッドを追加

--- spring_ensyuu/src/main/java/com/example/springensyuu/ScreenController.java
+++ spring_ensyuu/src/main/java/com/example/springensyuu/ScreenController.java
@@ -1,6 +1,7 @@
 package com.example.springensyuu;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -20,8 +21,19 @@ public class ScreenController {
 	}
 	
 	@GetMapping("/user/search")
-	public String searchUserName(@RequestParam int userId) {
-		helloService.getUserName(userId);
+	public String searchUserName(@RequestParam int userId, Model model) {
+		String userName = helloService.getUserName(userId);
+		model.addAttribute("userName", userName);	// addAttributeは配送依頼、第一引数が箱のラベル、第二引数に中の荷物。
 		return "viewName";
+	}
+	
+	/**
+	 * usersテーブルの内容を一覧表示するメソッドの作成途中。
+	 * @return
+	 */
+	@GetMapping("/screen/all")
+	public String screenAll() {
+		
+		return "all";
 	}
 }
--- spring_ensyuu/src/main/java/com/example/springensyuu/service/HelloService.java
+++ spring_ensyuu/src/main/java/com/example/springensyuu/service/HelloService.java
@@ -1,6 +1,8 @@
 package com.example.springensyuu.service;
 
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.example.springensyuu.entity.User;
@@ -19,7 +21,18 @@ public class HelloService {
 		return user.getUserName();		// userに格納されたレコードからgetUserNameでNameのみを返す。
 	}
 	
+	/**
+	 * usersテーブルに情報を登録する。
+	 * @param user
+	 */
 	public void addUser(User user) {
 		userRepository.save(user);
+	}
+	/**
+	 * usersテーブルの中身を一覧表示する。
+	 * @return
+	 */
+	public List<User> getAllUser() {
+		return userRepository.findAll();
 	}
 }
