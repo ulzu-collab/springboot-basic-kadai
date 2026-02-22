5d74719210a67124c3dad90e104d2552037b23ef
ulzu-collab <ulzu@outlook.jp>
2026-02-23 00:24:49 +0900

    ログイン機能試作

---
+++ spring_demo/src/main/java/com/example/spring_demo/controller/LoginController.java
@@ -0,0 +1,34 @@
+package com.example.spring_demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.spring_demo.service.UserService;
+
+@Controller
+public class LoginController {
+
+	private final UserService userService;
+
+	public LoginController(UserService userService) {
+		this.userService = userService;
+	}
+
+	@PostMapping("/login")
+	public String login(@RequestParam String email,
+			@RequestParam String password,
+			Model model) {
+
+		boolean result = userService.authenticate(email, password);
+
+		if (result) {
+			model.addAttribute("message", "ログイン成功！");
+			return "login-success";
+		} else {
+			model.addAttribute("message", "ログイン失敗…");
+			return "login-fail";
+		}
+	}
+}
